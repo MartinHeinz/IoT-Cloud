a6f9f71531be8faf7f60c8c3b160b475986f9b83
Martin Heinz <martin7.heinz@gmail.com>
2018-10-27 14:04:07 +0200

    Single app running both MQTT and flask endpoints + readme update

--- app/app/main.py
+++ app/app/main.py
@@ -1,8 +1,9 @@
-from app.app_setup import *
 from app.config import *
+from app.app_setup import *
 
 app = create_app(os.environ.get('FLASK_ENV'))
 
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, use_reloader=False, port=5000)
+
