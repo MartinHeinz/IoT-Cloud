018861436fc065566df7eb677779b11203f45b92
Martin Heinz <martin7.heinz@gmail.com>
2018-10-30 21:14:13 +0100

    Readme fixed; simple publish endpoint with blueprint; html msg and flash added to web; encrypted messaging from user to node-red device using publish endpoint

--- app/app/app_setup.py
+++ app/app/app_setup.py
@@ -10,38 +10,41 @@ from app.mqtt.mqtt import on_connect, on_message
 
 from app.config import config
 
-
 db = SQLAlchemy()
 client = mqtt.Client()
 
 
 def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    print("USING CONFIGURATION TYPE: " + config_name, flush=True)
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # not using sqlalchemy event system, hence disabling it
+	app = Flask(__name__)
+	app.config.from_object(config[config_name])
+	print("USING CONFIGURATION TYPE: " + config_name, flush=True)
+	app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+	# not using sqlalchemy event system, hence disabling it
 
-    config[config_name].init_app(app)
+	config[config_name].init_app(app)
 
-    # Set up extensions
-    db.init_app(app)
+	# Set up extensions
+	db.init_app(app)
 
-    # Create app blueprints
-    from app.web import web as web_blueprint
-    app.register_blueprint(web_blueprint)
+	# Create app blueprints
 
-    client.on_connect = on_connect
-    client.on_message = on_message
+	from app.api import api as api_blueprint
+	app.register_blueprint(api_blueprint, url_prefix="/api")
 
-    client.connect("192.168.0.102", 3883, 60)
-    print("Client connected...", flush=True)
+	from app.web import web as web_blueprint
+	app.register_blueprint(web_blueprint, url_prefix="/")
 
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(func=client.loop, trigger="interval", seconds=3)
-    scheduler.start()
+	client.on_connect = on_connect
+	client.on_message = on_message
 
-    # Shut down the scheduler when exiting the app
-    atexit.register(lambda: scheduler.shutdown())
+	client.connect(app.config["MQTT_BROKER_URL"], app.config["MQTT_BROKER_PORT"], 60)
+	print("Client connected...", flush=True)
 
-    return app
+	scheduler = BackgroundScheduler()
+	scheduler.add_job(func=client.loop, trigger="interval", seconds=3)
+	scheduler.start()
+
+	# Shut down the scheduler when exiting the app
+	atexit.register(lambda: scheduler.shutdown())
+
+	return app
