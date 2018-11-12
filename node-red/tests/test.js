84e6e6bdeb1e04d86504160d0999ffc3153e938d
Martin Heinz <martin7.heinz@gmail.com>
2018-11-12 21:11:26 +0100

    Now able to run node-RED inside mocha runtime and import flow from file.

--- node-red/tests/test.js
+++ node-red/tests/test.js
@@ -1,10 +1,38 @@
 // Run using npm test (mocha needs to be installed globally)
 
-var assert = require('assert');
-describe('Array', function () {
-	describe('#indexOf()', function () {
-		it('should return -1 when the value is not present', function () {
-			assert.equal([1, 2, 3].indexOf(4), -1);
-		});
+const assert = require('assert');
+const RED = require('node-red');
+const express = require("express");
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+const settings = {
+    httpAdminRoot:"/red",
+    httpNodeRoot: "/api",
+    userDir:"../",
+    flowFile: "../flow.json",
+    functionGlobalContext: { }    // enables global context
+};
+
+
+describe('Node-RED', function() {
+
+    before(function() {
+		RED.init(server, settings);
+		return RED.start();
+    });
+
+    after(function() {
+        RED.stop();
+    });
+
+    describe('Basic test', function () {
+        it('starts...', function (done) {
+			console.log(RED.nodes.getFlows());
+        	done();
+		})
 	});
 });
+
+// TODO: test - load existing flow; in "beforeEach" add nodes that inject some data into existing flow or make http request; check output of flow
