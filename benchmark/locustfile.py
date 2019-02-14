80d6a04b0a2f76bc5ba9f13d5c182c99260db7c8
Martin Heinz <martin7.heinz@gmail.com>
2019-02-14 21:17:55 +0100

    Locust webserver benchmark (WIP)

---
+++ benchmark/locustfile.py
@@ -0,0 +1,132 @@
+from locust import HttpLocust, TaskSet, task, TaskSequence, seq_task
+
+DEVICE_ID = 23
+DEVICE_NAME_BI = '$2b$12$1xxxxxxxxxxxxxxxxxxxxuZLbwxnpY0o58unSvIPxddLxGystU.Mq'
+ACCESS_TOKEN = "5c36ab84439c45a3719644c0d9bd7b31929afd9f"
+AA_ACCESS_TOKEN_USER_1 = '54agPr4edV9PvyyBNkjFfA))'
+AA_ACCESS_TOKEN_USER_2 = '7jagPr4edVdgvyyBNkjdaQ))'
+MASTER_KEY_USER_2 = 'eJyNUstOxDAM/JWo5x7iNC/zKwhVXVTBoQekLkhotf+Ox45hjxzSJs7Enhn7Nl326zY9hdu0rq/Hdp7rKqfp8n3dz2kOEv3ajs9do8+5z6HIqnUOlEgO2MQ0h8ZyyHMAohYJEm5l0+UyN9' \
+                    'njLyCWfYsAMJ42fBaBIdwMSkmwlUcSgJkts9ZUCB42ibQM2IKIUumeFOkrYb3cRcZbWrfj4/3fSks0peDVQRuUowobUYpSupdBl6LUYhCSQ19Mb1uMegcrGoxhSqEhMEV71avpqm4KbIKf' \
+                    'FfUF1LOn4V+Z2RsRByMQUcJlWNXkunpx7RPagWLMI4oMxdupHkOJnpAGUoiSNRW0G5muohfV3HloZlKjogMWGw0lUuxf/BGgelEtbfcuR7eouGhQivwwItSHDXAMliqWjTCS/TmTbfB8EN' \
+                    'UGtBcBomxPdY1xuf8AJSqbbw=='
+
+
+class AttrAuthUserBehavior(TaskSet):
+
+    @task()
+    def key_setup(self):
+        data = {"access_token": AA_ACCESS_TOKEN_USER_1}
+        with self.client.post("/attr_auth/setup",
+                              name="/attr_auth/setup",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+    @task()
+    def keygen(self):
+        data = {
+            "access_token": AA_ACCESS_TOKEN_USER_2,
+            "master_key": MASTER_KEY_USER_2,
+            "attr_list": "TODAY GUEST",
+            "receiver_id": "1"
+        }
+        with self.client.post("/attr_auth/keygen",
+                              name="/attr_auth/keygen",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+
+class AttrAuthUserEncryptAndDecrypt(TaskSequence):
+    ciphertext = None
+
+    @seq_task(1)
+    def encrypt(self):
+        data = {
+            "access_token": AA_ACCESS_TOKEN_USER_1,
+            "message": "any text",
+            "policy_string": "(GUESTTODAY)"
+        }
+        with self.client.post("/attr_auth/encrypt",
+                              name="/attr_auth/encrypt",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                self.ciphertext = response.json()["ciphertext"]
+                response.success()
+
+    @seq_task(2)
+    def decrypt(self):
+        data = {
+            "access_token": AA_ACCESS_TOKEN_USER_2,
+            "api_username": "MartinHeinz",
+            "ciphertext": self.ciphertext
+        }
+        with self.client.post("/attr_auth/decrypt",
+                              name="/attr_auth/decrypt",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+
+class UserBehavior(TaskSet):
+
+    @task()
+    def get_device_data(self):
+        data = {"device_id": DEVICE_ID, "access_token": ACCESS_TOKEN}
+        with self.client.post("/api/data/get_device_data",
+                              name="/api/data/get_device_data",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+    @task()
+    def get_device_by_name(self):
+        data = {"name_bi": DEVICE_NAME_BI, "access_token": ACCESS_TOKEN}
+        with self.client.post("/api/device/get",
+                              name="/api/device/get",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+    def create_device(self):
+        raise NotImplementedError
+
+    @task()
+    def get_data_by_num_range(self):
+        data = {"lower": "467297", "access_token": ACCESS_TOKEN, "device_id": DEVICE_ID}  # OPE - 2500
+        with self.client.post("/api/data/get_by_num_range",
+                              name="/api/data/get_by_num_range",
+                              params=data,
+                              verify=False,
+                              catch_response=True) as response:
+            if response.status_code == 200:
+                response.success()
+
+    def trigger_action(self):
+        raise NotImplementedError
+
+    def trigger_scene(self):
+        raise NotImplementedError
+
+    def exchange_session_keys(self):
+        raise NotImplementedError
+
+
+class WebsiteUser(HttpLocust):
+    task_set = UserBehavior  # Or AttrAuthUserBehavior or AttrAuthUserEncryptAndDecrypt
+    min_wait = 2000
+    max_wait = 5000
+
+    # Run using: locust --host=https://localhost
